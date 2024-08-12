604d19dba4320c4b71a47c1376e3c9b530a83576
Peng He <xnhp0320@gmail.com>
2024-08-12 10:22:33 +0800

    more exp for rvalue.

--- move.cc
+++ move.cc
@@ -10,17 +10,30 @@ public:
   S() {}
   S(const S&) {std::cout << "copied" << std::endl;}
   S(S&&) {std::cout << "moved" << std::endl;}
-  ~S() {}
+  ~S() {std::cout << "destruct " << std::endl;}
 };
+
+S return_S() {
+  return S();
+}
+
+S&& moved(S& s) {
+  return std::move(s);
+}
 
 int main() {
   S s;
   S s2 = std::move(s);
 
   [s3 = s2]() {
-    TD<decltype((s3))> xxx;
-    TD<decltype(s3)> xxx;
+    //TD<decltype((s3))> xxx;
+    //TD<decltype(s3)> xxx;
     S s_ = std::move(s3);
     (void)s_;
   }();
+
+  S s3 = S();
+  S&& s4 = moved(s3);
+  S s5 = s4;
+  std::cout << "S() is alive " << std::endl;
 }
